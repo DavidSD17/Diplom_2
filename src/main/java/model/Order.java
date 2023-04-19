dc9c8ea1e0d8eada996f335be0b64c1f9de730d2
David Sladkov <sda1217@gmail.com>
2023-04-19 22:29:22 +0300

    new schema generate order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,26 +1,21 @@
 package model;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Stack;
-
+import java.util.*;
+import model.SimpleArray;
 public class Order {
 
-     public String[] ingredients = {"61c0c5a71d1f82001bdaaa70","61c0c5a71d1f82001bdaaa72","61c0c5a71d1f82001bdaaa75",
-             "61c0c5a71d1f82001bdaaa76","61c0c5a71d1f82001bdaaa78","61c0c5a71d1f82001bdaaa79","61c0c5a71d1f82001bdaaa6f","61c0c5a71d1f82001bdaaa6d"};
+      SimpleArray simpleArray = new SimpleArray();
+
+    public String[] ingredients = Arrays.copyOf(simpleArray.getSimpleArray(),new Random().nextInt(8 - 2) + 2);
 
 
-    public Order(String[] ingredients){
-       this.ingredients = ingredients;
-   }
-
-    public String[] CreateOrder() {
-        return ingredients;
-    }
     public Order(){
 
     }
 
+
+    public Order(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
 }
---
+++ src/main/java/model/SimpleArray.java
@@ -0,0 +1,14 @@
+package model;
+
+public class SimpleArray {
+   protected String[] simpleArray = {"61c0c5a71d1f82001bdaaa70","61c0c5a71d1f82001bdaaa72","61c0c5a71d1f82001bdaaa75",
+            "61c0c5a71d1f82001bdaaa76","61c0c5a71d1f82001bdaaa78","61c0c5a71d1f82001bdaaa79","61c0c5a71d1f82001bdaaa6f","61c0c5a71d1f82001bdaaa6d"};
+
+   public String[] getSimpleArray() {
+      return simpleArray;
+   }
+
+   public void setSimpleArray(String[] simpleArray) {
+      this.simpleArray = simpleArray;
+   }
+}
